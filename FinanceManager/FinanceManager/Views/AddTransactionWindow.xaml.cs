ed81f8f0b488973c8f01e5141ccdbb0358675520
Дмитрий Лёвкин <jonikm892@gmail.com>
2026-03-06 23:59:06 +0300

    Добавлено окно добавления транзакций

---
+++ FinanceManager/FinanceManager/Views/AddTransactionWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using FinanceManager.Services;
+using FinanceManager.ViewModels;
+
+namespace FinanceManager.Views
+{
+    public partial class AddTransactionWindow : Window
+    {
+        public AddTransactionWindow(DataService dataService)
+        {
+            InitializeComponent();
+            var vm = new TransactionViewModel(dataService);
+            vm.RequestClose += () => this.Close();
+            DataContext = vm;
+        }
+    }
+}
--- FinanceManager/FinanceManager/views/MainWindow.xaml.cs
+++ FinanceManager/FinanceManager/views/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using FinanceManager.ViewModels;
 
 namespace FinanceManager.Views
@@ -17,7 +8,6 @@ namespace FinanceManager.Views
         public MainWindow()
         {
             InitializeComponent();
-            // VM держит всю логику/данные, окно остаётся тонким
             DataContext = new MainViewModel();
         }
     }
