122b2c6ea2eb669d6fc999869976dfb9c2cea236
Ereb <stitch2277@gmail.com>
2026-03-06 18:16:56 +0300

    Создал Transaction model, TransactionType enum, RelayCommand, BaseViewModel

---
+++ FinanceManager/FinanceManager/Models/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceManager.Models
+{
+    public class Transaction
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Category { get; set; }
+        public TransactionType Type { get; set; }
+        public string Comment { get; set; } = "";
+    }
+}
